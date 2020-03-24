b7b8f59c13fb49cf0281efaee975b2dcbc143f88
Rofandi <33696179+Rofandi@users.noreply.github.com>
2020-03-24 15:37:13 +0700

    kanban heroku 3

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,11 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "postgres",
-    "url": process.env.DATABASE_URL
-  }
+  },
+
 }
